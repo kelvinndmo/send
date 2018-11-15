e48e4a670efa1cc8e176be5cc16c15771198f22f
novak254 <ndemokelvinonkundi@gmail.com>
2018-11-15 05:32:31 +0300

    [feature #161834165] validators for the input fields

--- utils/validators.py
+++ utils/validators.py
@@ -5,8 +5,7 @@ class Validators:
         ''' validate destination name '''
         regex = "^[a-zA-Z0-9_ ]{1,}$"
         return re.match(regex, destination)
-
-
+  
     def valid_origin_name(self,origin):
         ''' validate origin name '''
         regex = "^[a-zA-Z0-9_ ]{2,}$"
@@ -14,34 +13,13 @@ class Validators:
     
     def valid_name(self, username):
         """ Valid username """
-        return re.match("^[a-zA-Z0-9]{6,}$", username)
+        return re.match("^[a-zA-Z0-9]{2,}$", username)
 
     def valid_password(self, password):
         """validate for password """
-        # positive look ahead
-        return re.match("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9]{8,15}$",
+        return re.match("^[a-zA-Z0-9]{2,15}$",
                         password)
 
     def valid_email(self, email):
         """ validate for email """
         return re.match("^[^@]+@[^@]+\.[^@]+$", email)
-
-    def valid_inputs(self, string_inputs):
-        """ validate for inputs """
-        return re.match("^[a-zA-Z0-9-\._@ ]+$", string_inputs)
-        # return re.match("^[a-zA-Z0-9-\._@ ]+$", string_inputs)
-
-    def valid_is_admin(self, is_admin):
-        """ validate is_admin """
-        return re.match("^[0-1]{,1}$", is_admin)
-
-
-    # def valid_person_name(customer_name):
-    #     '''validate person's name'''
-    #     regex = "^[a-zA-Z ]{4,}$"
-    #     return re.match(regex, customer_name)
-
-    # def valid_destination(destination):
-    #     '''validate destination name'''
-    #     regex = "^[a-zA-Z 0-9]{3,}$"
-    #     return re.match(regex, destination)
