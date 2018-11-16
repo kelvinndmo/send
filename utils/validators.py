082a5d738417b00b7285555f9e5f78710be55023
kelvin onkundi ndemo <32582512+kelvinndmo@users.noreply.github.com>
2018-11-16 11:04:10 +0300

    added validators

--- utils/validators.py
+++ utils/validators.py
@@ -3,21 +3,21 @@ import re
 class Validators:
     def valid_destination_name(self,destination):
         ''' validate destination name '''
-        regex = "^[a-zA-Z0-9_ ]{1,}$"
+        regex = "^(?=.*[a-z])[a-zA-Z0-9]{8,15}$"
         return re.match(regex, destination)
   
     def valid_origin_name(self,origin):
         ''' validate origin name '''
-        regex = "^[a-zA-Z0-9_ ]{2,}$"
+        regex ="^(?=.*[a-z])[a-zA-Z0-9]{8,15}$"
         return re.match(regex, origin)
     
     def valid_name(self, username):
         """ Valid username """
-        return re.match("^[a-zA-Z0-9]{2,}$", username)
+        return re.match("^(?=.*[a-z])[a-zA-Z0-9]{8,15}$", username)
 
     def valid_password(self, password):
         """validate for password """
-        return re.match("^[a-zA-Z0-9]{2,15}$",
+        return re.match("^(?=.*[a-z])[a-zA-Z0-9]{8,15}$",
                         password)
 
     def valid_email(self, email):
