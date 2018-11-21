e5bbd7b3db7200f50194e69ee3b6dd8d837028fa
novak254 <ndemokelvinonkundi@gmail.com>
2018-11-22 02:44:33 +0300

    [feature #161834220] tests for the app

--- tests/test_customers.py
+++ tests/test_customers.py
@@ -26,7 +26,7 @@ class TestPostParcel(BaseTest):
         self.assertEqual(response.status_code, 400)
 
     def test_get_parcel_order_does_not_exist(self):
-        """ Test food order does not exist """
+        """ Test parcel order does not exist """
         token = self.get_token_as_user()
 
         response = self.client.get(
@@ -67,6 +67,134 @@ class TestPostParcel(BaseTest):
         )
 
         self.assertEqual(response.status_code, 200)
+    def test_get_all_order(self):
+        """test for returning all orders"""
+
+        token = self.get_token_as_user()
+
+        self.post_parcel()
+
+        response = self.client.get(
+            "api/v2/parcels",
+             headers={'content-type': 'application/json',
+                     'Authorization': f'Bearer {token}'}
+        )
+
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_one_order(self):
+        """test for get one one order"""
+
+        token = self.get_token_as_user()
+
+        self.post_parcel()
+
+        response = self.client.get(
+            "api/v2/parcels/1",
+             headers={'content-type': 'application/json',
+                     'Authorization': f'Bearer {token}'}
+        )
+
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_one_order_non_exist(self):
+        """tes for orders non exsitent orders"""
+
+        token = self.get_token_as_user()
+
+        self.post_parcel()
+
+        response = self.client.get(
+            "api/v2/parcels/100000",
+             headers={'content-type': 'application/json',
+                     'Authorization': f'Bearer {token}'}
+        )
+
+        self.assertEqual(response.status_code, 404)
+    
+    def test_get_intransit_orders(self):
+        """test for returning all orders"""
+
+        token = self.get_token_as_user()
+
+        self.post_parcel()
+        self.accept_order()
+        self.mark_in_transit_order()
+
+        response = self.client.get(
+            "api/v2/parcels/intransit",
+             headers={'content-type': 'application/json',
+                     'Authorization': f'Bearer {token}'}
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_declined_orders(self):
+        """test for returning all declined orders"""
+
+        token = self.get_token_as_user()
+
+        self.post_parcel()
+        self.reject_order()
+
+        response = self.client.get(
+            "api/v2/parcels/declined",
+             headers={'content-type': 'application/json',
+                     'Authorization': f'Bearer {token}'}
+        )
+
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_no_declined_orders(self):
+        """test for returning no declined  orders"""
+
+        token = self.get_token_as_user()
+
+        self.post_parcel()
+
+        response = self.client.get(
+            "api/v2/parcels/declined",
+             headers={'content-type': 'application/json',
+                     'Authorization': f'Bearer {token}'}
+        )
+
+        self.assertEqual(response.status_code, 404)
+    
+    def test_get_completed_orders(self):
+        """test for returning all orders"""
+
+        token = self.get_token_as_user()
+
+        self.post_parcel()
+        self.accept_order()
+        self.mark_in_transit_order()
+        self.complete_order()
+
+        response = self.client.get(
+            "api/v2/parcels/completedorders",
+             headers={'content-type': 'application/json',
+                     'Authorization': f'Bearer {token}'}
+        )
+
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_no_completed_orders(self):
+        """test for returning all orders"""
+
+        token = self.get_token_as_user()
+
+        self.post_parcel()
+        self.accept_order()
+        self.mark_in_transit_order()
+
+        response = self.client.get(
+            "api/v2/parcels/completedorders",
+             headers={'content-type': 'application/json',
+                     'Authorization': f'Bearer {token}'}
+        )
+
+        self.assertEqual(response.status_code, 404)
+
 
     def test_accepted_parcel_order_does_not_exist(self):
         """ test accepted parcel orders does not exist """
@@ -96,6 +224,39 @@ class TestPostParcel(BaseTest):
         )
 
         self.assertEqual(response.status_code, 200)
+    
+    def test_cancel_order(self):
+        '''test for user cancelling an order'''
+
+        token = self.get_token_as_user()
+
+        self.post_parcel()
+        
+        res = self.client.put(
+            "api/v2/parcels/1/cancel",
+            headers={'content-type': 'application/json',
+                     'Authorization': f'Bearer {token}'}
+        )
+
+        self.assertEqual(res.status_code, 200)
+    
+    def test_cancel_order(self):
+        '''test for user cancelling an order'''
+
+        token = self.get_token_as_user()
+
+        self.post_parcel()
+        self.cancel_order()
+        
+        res = self.client.put(
+            "api/v2/parcels/1/cancel",
+            headers={'content-type': 'application/json',
+                     'Authorization': f'Bearer {token}'}
+        )
+
+        self.assertEqual(res.status_code, 400)
+        
+
 
     def test_mark_parcel_in_transit(self):
         token = self.get_token_as_admin()
@@ -155,16 +316,7 @@ class TestPostParcel(BaseTest):
 
         self.assertEqual(response.status_code, 400)
 
-    # def test_mark_parcel_in_transit_already_rejected(self):
-    #     token = self.get_token_as_admin()
-
-       
-    #     self.post_parcel()
-    #     self.reject_order()
-
-    #     response = self.mark_in_transit_order()
-    #     print(response.data)
-    #     self.assertEqual(response.status_code, 400)
+   
 
     def test_rejected_parcel_orders_does_not_exist(self):
         """ test rejected declined orders does not exist """
@@ -255,3 +407,6 @@ class TestPostParcel(BaseTest):
                      'Authorization': f'Bearer {token}'}
         )
         self.assertEqual(response.status_code, 404)
+    
+   
+        
