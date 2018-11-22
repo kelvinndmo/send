304ad98a5a3fab6b0fc47536f438ac66d034e8c3
novak254 <ndemokelvinonkundi@gmail.com>
2018-11-22 09:08:20 +0300

    updated manage.py

--- manage.py
+++ manage.py
@@ -1,18 +1,4 @@
 from run import app
-from models.models import User, Parcel
-
-def create():
-    User().create_table()
-    Parcel().create_table()
-
-def drop():
-    User().drop_table()
-    Parcel().drop_table()
-
-def create_admin():
-    admin = User(username='AdminUser', email='admin@gmail.com',
-                 password='Adminpass123', is_admin=True)
-    admin.add()
 
 
 if __name__ == '__main__':
