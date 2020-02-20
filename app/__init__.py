f499757b768092e3b8b0e1fcac6ba47e4d0edbfe
kelvin onkundi ndemo <32582512+kelvinndmo@users.noreply.github.com>
2020-02-20 03:08:22 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ def create_app(config_stage):
     app = Flask(__name__)
     app.config.from_object(app_config[config_stage])
     
-    CORS(app,resources={r"/api/*": {"origins": "http://localhost:3000"}})
+    CORS(app)
     
 
     jwt.init_app(app)
